f16b1ac7957c4738f48a61a5f343aa9f46428cc5
kingapl <kingapluciennik@wp.pl>
2021-01-24 22:14:50 +0100

    Dice roll simulator

---
+++ dice_roll_simulator.py
@@ -0,0 +1,42 @@
+import tkinter as tk
+from PIL import ImageTk, Image
+
+from random import choice
+
+dice = ["images/dice1.png", "images/dice2.png", "images/dice3.png", 
+        "images/dice4.png", "images/dice5.png", "images/dice6.png"]
+
+class DiceRollSimulator(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.master = master
+        master.geometry("600x400")
+        master.title("Dice Roll Simulator")
+
+        self.pack()
+        self.create_widgets()
+
+
+    def create_widgets(self):
+        self.text_label = tk.Label(self, text="Hello, roll the dice!")
+        self.text_label.pack(side="top")
+
+        self.roll_the_dice = tk.Button(self, text="Roll the dice", command=self.dice_roll)
+        self.roll_the_dice.pack()
+        self.label = tk.Label(self, image="")
+
+
+    def dice_roll(self):
+        die = choice(dice)
+
+        self.original = Image.open(die) # 640x628
+        resized = self.original.resize((320, 314), Image.ANTIALIAS)
+        self.image = ImageTk.PhotoImage(resized)
+        #self.label = tk.Label(self, image=self.image)
+        self.label.config(image=self.image)
+        self.label.pack()
+
+
+root = tk.Tk()
+dice_roll_simulator = DiceRollSimulator(master=root)
+dice_roll_simulator.mainloop()
